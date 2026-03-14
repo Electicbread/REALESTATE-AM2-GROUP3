1f89abc0fdaeaef5c8ec061973b282b4c4ac33cc
Electicbread <ethanestiva.shs@gmail.com>
2026-03-14 16:06:47 +0800

    Add files via upload

---
+++ RealEstateGroup3/src/MyLib/Property.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MyLib;
+
+/**
+ *
+ * @author argee
+ */
+public abstract class Property {
+    private double price;
+    private String location;
+    private String block;
+    private String lot;
+    private String status;
+    private int size;
+
+    public Property(double price, String location, String block, String lot, String status, int size) {
+        this.price = price;
+        this.location = location;
+        this.block = block;
+        this.lot = lot;
+        this.status = status;
+        this.size = size;
+    }
+    
+    public abstract void getFeatures();
+    
+}
---
+++ RealEstateGroup3/src/MyLib/User.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MyLib;
+
+/**
+ *
+ * @author argee
+ */
+public class User {
+    private String name;
+    private String telNum;
+
+    public User(String name, String telNum) {
+        this.name = name;
+        this.telNum = telNum;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTelNum() {
+        return telNum;
+    }
+    
+    
+    
+}
