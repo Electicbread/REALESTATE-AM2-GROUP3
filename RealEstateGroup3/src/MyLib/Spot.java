638cd1956886a3932374441aaa0d15d787608a38
ysynthey <ysyntheofficialbuisness@gmail.com>
2026-03-14 21:24:13 +0800

    added Spot.java
    Copilot is NOT THINKIUNG

---
+++ RealEstateGroup3/src/MyLib/Spot.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MyLib;
+
+/**
+ *
+ * @author Synthe
+ */
+public class Spot extends PaymentPlan{
+    private float discount;
+    private int days;
+
+    public Spot(Property property, double reservedFee, float discount, int days) {
+        super(property, reservedFee);
+        this.discount = discount;
+        this.days = days;
+    }
+    
+    @Override
+    public double calculatePrice() {
+        return super.reservedFee * discount; //i forgot what the base price is i mean we havent added the full calculation logic
+    }
+    
+    
+}
