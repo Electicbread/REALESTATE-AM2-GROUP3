a765f6a6317d7b34f63117dff5ef1b4db0bd3e3e
nmmorco7-max <nmmorco7@gmail.com>
2026-03-17 20:20:40 +0800

    Update PaymentPlan.java

--- RealEstateGroup3/src/MyLib/PaymentPlan.java
+++ RealEstateGroup3/src/MyLib/PaymentPlan.java
@@ -9,8 +9,6 @@ package MyLib;
  * @author Synthe
  */
 
-import java.util.Scanner; 
-
 public abstract class PaymentPlan {
     protected Property property;
     protected double reservedFee;
@@ -23,32 +21,13 @@ public abstract class PaymentPlan {
     public abstract void viewReceipt();
     public abstract double calculatePrice();
 
-    public static PaymentPlan selectPaymentPlan(Property property, int choice) {
-        Scanner scanner = new Scanner(System.in);
-
+    public static PaymentPlan selectPaymentPlan(Property property, int choice, double reservedFee, int days) {
         switch (choice) {
             case 1:
-                System.out.println("\n-- SPOT PAYMENT --");
-                System.out.print("Enter Reservation Fee: ");
-                double spotReservedFee = scanner.nextDouble();
-                System.out.print("Enter Discount (e.g. 0.05 for 5%): ");
-                float discount = scanner.nextFloat();
-                System.out.print("Enter Number of Days: ");
-                int days = scanner.nextInt();
-                return new Spot(property, spotReservedFee, discount, days);
-
+                return new Spot(property, reservedFee, days);
             case 2:
-                System.out.println("\n-- INSTALLMENT --");
-                System.out.print("Enter Reservation Fee: ");
-                double instReservedFee = scanner.nextDouble();
-                System.out.print("Enter Interest Rate (e.g. 0.06 for 6%): ");
-                float interest = scanner.nextFloat();
-                System.out.print("Enter Number of Months: ");
-                int months = scanner.nextInt();
-                return new Installment(interest, months, property, instReservedFee);
-
+                return new Installment(property, reservedFee);
             default:
-                System.out.println("Invalid choice!");
                 return null;
         }
     }
