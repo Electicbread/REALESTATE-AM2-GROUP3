843195338f7ed75957b8378439f3520308766745
Electicbread <ethanestiva.shs@gmail.com>
2026-03-17 20:39:48 +0800

    Update SingleAttached.java

--- RealEstateGroup3/src/MyLib/SingleAttached.java
+++ RealEstateGroup3/src/MyLib/SingleAttached.java
@@ -9,15 +9,17 @@ package MyLib;
  * @author Synthe
  */
 public class SingleAttached extends Property{
-    private boolean twoFloor;
-
-    public SingleAttached(double price, String location, String block, String lot, String status, int size, boolean twoFloor) {
-        super(price, location, block, lot, status, size);
-        this.twoFloor = twoFloor;
+   public SingleAttached(double price, String location, String block, String lot, int size) {
+        super(price, location, block, lot, size);
+  
     }
     
     @Override
     public void getFeatures() {
-        
+        System.out.println("Price: " + super.price);
+        System.out.println("Location: " + super.location);
+        System.out.println("Block: " + super.block);
+        System.out.println("Lot: " + super.lot);
+        System.out.println("size: " + super.size + " sqm");
     }
 }
